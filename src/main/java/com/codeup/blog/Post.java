5fa444820f18cb8d220c096c0aa40050349aa435
Jennifer Walker <dataqueen16@gmail.com>
2018-06-19 10:23:47 -0500

    set up two html pages to see posts and modified the PostController methods to pass the data

--- src/main/java/com/codeup/blog/Post.java
+++ src/main/java/com/codeup/blog/Post.java
@@ -4,7 +4,7 @@ public class Post {
     private String title;
     private String body;
 
-    public Post(){
+    public Post(String title, String body){
         this.title = title;
         this.body = body;
     }
--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -3,24 +3,33 @@ package com.codeup.blog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
     public @ResponseBody String post() {
-        return "Posts index page";
+        return "create a post here";
     }
 
-    @GetMapping("/posts/{id}")
+    @GetMapping("/posts/index/{id}")
     @ResponseBody
-    public String postID(@PathVariable int id) {
-        return "View an individual post at " + id;
+    public Post postID(@PathVariable int id) {
+        Post blog = new Post("Women in Tech", "We are here. We won't go away. We will only get stronger my friend.");
+        return blog;
     }
 
-    @RequestMapping(path="/posts/create", method= RequestMethod.GET)
+    @RequestMapping(path="/posts/show", method= RequestMethod.GET)
     @ResponseBody
-    public String generatePosts() {
-        return "View the post form";
+    public List<Post> generatePosts(@PathVariable List<Post> posts) {
+        posts = new ArrayList<>();
+        Post blog = new Post("How to kiss a girl", "Be sweet and attentive. Get in real close. Kiss her lips like they are delicious strawberries that you cannot get enough of.");
+        Post queer = new Post("Love is ours too", "Creating a fair, equal, and fun society.");
+        posts.add(blog);
+        posts.add(queer);
+        return posts;
     }
 
     @RequestMapping(path="/posts/create", method = RequestMethod.POST)
